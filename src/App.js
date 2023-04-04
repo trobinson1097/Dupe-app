8c2db3e8db229e1c11a239433287590fd8e3048d
Tiana Robinson <trobinson1097@gmail.com>
2023-04-04 10:11:19 -0700

    replicating layout of anoother site

--- src/App.js
+++ src/App.js
@@ -4,20 +4,75 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <html lang="en" >
+        <div className='App1' >
+          <div className='firstBloc'>
+        <div _ngcontent-fwy-c1="" class="new_pop_up_container" >
+          <div _ngcontent-fwy-c1="" class="half_container"><div _ngcontent-fwy-c1="">
+            <div _ngcontent-fwy-c1="">
+              <svg _ngcontent-fwy-c1="" data-name="Group 26" height="50" viewBox="0 0 86 86" width="50" xmlns="http://www.w3.org/2000/svg">
+                <defs _ngcontent-fwy-c1="">
+                  <clipPath _ngcontent-fwy-c1="" id="yecorx474a">
+                    <path _ngcontent-fwy-c1="" class="svg_video_circle" d="M0 0h86v86H0z" data-name="Rectangle 1">
+                    </path>
+                  </clipPath>
+                </defs>
+                <g _ngcontent-fwy-c1="" data-name="Group 1" >
+                  <path _ngcontent-fwy-c1="" class="svg_video_circle" d="M86 42.7a43 43 0 1 0-43 42.8 42.108 42.108 0 0 0 14.333-2.494L77.179 86h.4a2.744 2.744 0 0 0 1.9-.8 2.424 2.424 0 0 0 .6-2.294l-3.401-13.567a45.928 45.928 0 0 0 6.515-11.074A45.474 45.474 0 0 0 86 42.7zm-7.618 13.769a35.6 35.6 0 0 1-6.515 10.676 2.455 2.455 0 0 0-.5 2.195l2.807 11.174-16.74-2.495h-.4a2.927 2.927 0 0 0-.9.2A37.262 37.262 0 0 1 43 80.613a37.971 37.971 0 1 1 35.383-24.145z" data-name="Path 1">
+                  </path>
+                  <path _ngcontent-fwy-c1="" class="svg_video_circle" d="M125.232 189.834a3.569 3.569 0 0 1-3.573-3.558v-21.961a3.532 3.532 0 0 1 1.048-2.514 3.568 3.568 0 0 1 2.526-1.044h23.116a3.567 3.567 0 0 1 2.524 1.043 3.528 3.528 0 0 1 1.047 2.512v5.221l9.721-5.589a2.121 2.121 0 0 1 3.183 1.831v19.039a2.125 2.125 0 0 1-3.184 1.83l-9.722-5.589v5.222a3.569 3.569 0 0 1-3.572 3.556zm.671-4.225h21.773v-20.632H125.9zm26.73-11.615a1.753 1.753 0 0 1-.228.111l-.485.2v1.98l.488.194a1.81 1.81 0 0 1 .231.111l7.942 4.57v-11.728l-6.777 3.894v-.012z" data-name="Path 2" transform="translate(-99.884 -132.117)">
+                  </path>
+                </g>
+              </svg>
+            </div>
+            <div _ngcontent-fwy-c1="" class="header_text"> Talk to an expert </div>
+          </div>
+            <div _ngcontent-fwy-c1="" class="sub_text"> Sorry, we’re busy right now.  Hit the button below to book a call or try again later. </div>
+            <div _ngcontent-fwy-c1="">
+            </div>
+            <div _ngcontent-fwy-c1="">
+              <button _ngcontent-fwy-c1="" class="new_buttons"> Book for later </button>
+            </div>
+            <div _ngcontent-fwy-c1="">
+              <div _ngcontent-fwy-c1="" class="privacy_text" id="privacyText">
+                <p class="info-cla-privacy" >By clicking this button you acknowledge and accept the following <a href="https://eyekandy.com/privacy" target="_blank">Privacy Policy</a> and <a  href="https://eyekandy.com/terms-and-conditions-live-shopping/" target="_blank">Terms Conditions</a>
+                </p>
+                </div>
+                </div>
+            </div>
+          </div>
+          <div _ngcontent-fwy-c1="" class="middle_divider"></div>
+          <div _ngcontent-fwy-c1="" class="half_container">
+            <div _ngcontent-fwy-c1="">
+              <div _ngcontent-fwy-c1="">
+                <svg _ngcontent-fwy-c1="" height="60" viewBox="0 0 63 63" width="60" xmlns="http://www.w3.org/2000/svg">
+                  <g _ngcontent-fwy-c1="" data-name="Group 407" transform="translate(-1770 -940)">
+                    <g _ngcontent-fwy-c1="" data-name="Group 21">
+                      <g _ngcontent-fwy-c1="" data-name="Group 15">
+                        <g _ngcontent-fwy-c1="" data-name="Group 14" >
+                          <path _ngcontent-fwy-c1="" class="svg_video_circle" d="M141.76 291.687a1.174 1.174 0 0 0-1.473.73 4.731 4.731 0 0 1-4.408 3.01h-.314a4.732 4.732 0 0 1-4.407-3.01 1.176 1.176 0 0 0-2.224.764l.009.024a7.18 7.18 0 0 0 6.689 4.569h.214a7.181 7.181 0 0 0 6.689-4.57l.006-.015.005-.016a1.174 1.174 0 0 0-.785-1.487" data-name="Path 3" transform="translate(-121.551 -275.535)">
+                          </path>
+                          <path _ngcontent-fwy-c1="" class="svg_video_circle" d="M26.587 8.531a13.818 13.818 0 1 0 1.052 5.288 13.753 13.753 0 0 0-1.052-5.288m-12.768 16.76a11.471 11.471 0 1 1 4.39-.873 11.414 11.414 0 0 1-4.39.873" data-name="Path 4" transform="translate(.336 .29)">
+                          </path><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M107.346 143.7a1.369 1.369 0 0 1 1.039-1.355c.573 0 1.039.82 1.039 1.355a1.174 1.174 0 1 0 2.348 0 3.947 3.947 0 0 0-.938-2.489 3.077 3.077 0 0 0-4.9 0A3.947 3.947 0 0 0 105 143.7a1.174 1.174 0 1 0 2.348 0" data-name="Path 5" transform="translate(-98.972 -132.121)"></path><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M285.834 141.214a3.077 3.077 0 0 0-4.9 0A3.947 3.947 0 0 0 280 143.7a1.174 1.174 0 1 0 2.348 0 1.369 1.369 0 0 1 1.039-1.355c.573 0 1.039.82 1.039 1.355a1.174 1.174 0 1 0 2.348 0 3.947 3.947 0 0 0-.938-2.489" data-name="Path 6" transform="translate(-264.488 -132.123)"></path></g></g><g _ngcontent-fwy-c1="" data-name="Group 16"><g _ngcontent-fwy-c1="" data-name="Group 14"  transform="translate(1770.853 971.028)"><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M141.76 291.687a1.174 1.174 0 0 0-1.473.73 4.731 4.731 0 0 1-4.408 3.01h-.314a4.732 4.732 0 0 1-4.407-3.01 1.176 1.176 0 0 0-2.224.764l.009.024a7.18 7.18 0 0 0 6.689 4.569h.214a7.181 7.181 0 0 0 6.689-4.57l.006-.015.005-.016a1.174 1.174 0 0 0-.785-1.487" data-name="Path 3" transform="translate(-121.279 -275.442)"></path><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M26.587 8.531a13.818 13.818 0 1 0 1.052 5.288 13.753 13.753 0 0 0-1.052-5.288m-12.768 16.76a11.471 11.471 0 1 1 4.39-.873 11.414 11.414 0 0 1-4.39.873" data-name="Path 4" transform="translate(.608 .382)"></path><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M107.346 143.7a1.369 1.369 0 0 1 1.039-1.355c.573 0 1.039.82 1.039 1.355a1.174 1.174 0 1 0 2.348 0 3.947 3.947 0 0 0-.938-2.489 3.077 3.077 0 0 0-4.9 0A3.947 3.947 0 0 0 105 143.7a1.174 1.174 0 1 0 2.348 0" data-name="Path 5" transform="translate(-98.7 -132.029)"></path><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M285.834 141.214a3.077 3.077 0 0 0-4.9 0A3.947 3.947 0 0 0 280 143.7a1.174 1.174 0 1 0 2.348 0 1.369 1.369 0 0 1 1.039-1.355c.573 0 1.039.82 1.039 1.355a1.174 1.174 0 1 0 2.348 0 3.947 3.947 0 0 0-.938-2.489" data-name="Path 6" transform="translate(-264.216 -132.031)"></path></g></g><g _ngcontent-fwy-c1="" data-name="Group 17"><g _ngcontent-fwy-c1="" data-name="Group 14" transform="translate(1804.831 971.028)">
+                          <path _ngcontent-fwy-c1="" class="svg_video_circle" d="M141.76 291.687a1.174 1.174 0 0 0-1.473.73 4.731 4.731 0 0 1-4.408 3.01h-.314a4.732 4.732 0 0 1-4.407-3.01 1.176 1.176 0 0 0-2.224.764l.009.024a7.18 7.18 0 0 0 6.689 4.569h.214a7.181 7.181 0 0 0 6.689-4.57l.006-.015.005-.016a1.174 1.174 0 0 0-.785-1.487" data-name="Path 3" transform="translate(-121.823 -275.442)"></path><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M26.587 8.531a13.818 13.818 0 1 0 1.052 5.288 13.753 13.753 0 0 0-1.052-5.288m-12.768 16.76a11.471 11.471 0 1 1 4.39-.873 11.414 11.414 0 0 1-4.39.873" data-name="Path 4" transform="translate(.064 .382)"></path><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M107.346 143.7a1.369 1.369 0 0 1 1.039-1.355c.573 0 1.039.82 1.039 1.355a1.174 1.174 0 1 0 2.348 0 3.947 3.947 0 0 0-.938-2.489 3.077 3.077 0 0 0-4.9 0A3.947 3.947 0 0 0 105 143.7a1.174 1.174 0 1 0 2.348 0" data-name="Path 5" transform="translate(-99.244 -132.029)"></path><path _ngcontent-fwy-c1="" class="svg_video_circle" d="M285.834 141.214a3.077 3.077 0 0 0-4.9 0A3.947 3.947 0 0 0 280 143.7a1.174 1.174 0 1 0 2.348 0 1.369 1.369 0 0 1 1.039-1.355c.573 0 1.039.82 1.039 1.355a1.174 1.174 0 1 0 2.348 0 3.947 3.947 0 0 0-.938-2.489" data-name="Path 6" transform="translate(-264.76 -132.031)"></path></g>
+                      </g>
+                    </g></g>
+                </svg></div>
+              <div _ngcontent-fwy-c1="" class="header_text" > Shop with friends </div>
+            </div>
+            <div _ngcontent-fwy-c1="" class="sub_text"> Invite your friends and family to shop with you LIVE over video call </div>
+            <form _ngcontent-fwy-c1="" novalidate="" class="ng-untouched ng-pristine ng-invalid">
+              <input _ngcontent-fwy-c1="" class="swf_username ng-untouched ng-pristine ng-invalid" formcontrolname="UserName" type="text" placeholder="Enter your name" />
+            </form>
+            <div _ngcontent-fwy-c1=""><button _ngcontent-fwy-c1="" class="new_buttons"> Start shopping! </button>
+            </div>
+            <div _ngcontent-fwy-c1="">
+              <div _ngcontent-fwy-c1="" class="footer_text"> You will get an invite link once you submit your name </div>
+            </div>
+          </div>
+          </div>
+          </div>
+      </html>
     </div>
   );
 }
